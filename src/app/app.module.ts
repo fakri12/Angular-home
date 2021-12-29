d3cc0913482028cf75240fc5b3bf01f16ba942d5
ball ball <salahdinetiger@gmail.com>
2021-12-29 02:41:38 +0100

    I added Search in home and Welcoming part in home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpNavBarComponent } from './component/up-nav-bar/up-nav-bar.component';
 import { DownNavBarComponent } from './component/down-nav-bar/down-nav-bar.component';
+import { SearchHomeComponent } from './component/search-home/search-home.component';
+import { FormsModule } from '@angular/forms';
+import { WelcomeHomeComponent } from './component/welcome-home/welcome-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpNavBarComponent,
-    DownNavBarComponent
+    DownNavBarComponent,
+    SearchHomeComponent,
+    WelcomeHomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
