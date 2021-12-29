2e587dfd288c2f0e2929fc6839c633c2a1cb2729
fakri <medalifakri2000@gmail.com>
2021-12-29 19:34:32 +0100

    list of all the componant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpNavBarComponent } from './component/up-nav-bar/up-nav-bar.component';
 import { DownNavBarComponent } from './component/down-nav-bar/down-nav-bar.component';
+import { CarComponentComponent } from './component/car-component/car-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpNavBarComponent,
-    DownNavBarComponent
+    DownNavBarComponent,
+    CarComponentComponent
   ],
   imports: [
     BrowserModule,
