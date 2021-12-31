1f21206668ce8cd66822243272d5cc96dd44f21c
houssam <houssamelouatr@gmail.com>
2021-12-31 21:30:28 +0100

    Slider-logos and payment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UpNavBarComponent } from './component/up-nav-bar/up-nav-bar.component';
 import { DownNavBarComponent } from './component/down-nav-bar/down-nav-bar.component';
+import { LogosSlideComponent } from './component/logos-slide/logos-slide.component';
+import { PaymentComponent } from './component/payment/payment.component';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpNavBarComponent,
-    DownNavBarComponent
+    DownNavBarComponent,
+    LogosSlideComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/logos-slide/logos-slide.component.ts
+++ src/app/component/logos-slide/logos-slide.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-logos-slide',
+  templateUrl: './logos-slide.component.html',
+  styleUrls: ['./logos-slide.component.css']
 })
-export class AppComponent {
-  title = 'POS';
+export class LogosSlideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
